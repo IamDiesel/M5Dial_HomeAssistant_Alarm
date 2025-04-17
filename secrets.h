569e8bdf9fe148f4d7b729fa3bc0ba19efe5290a
IamDiesel <Daniel.Kahrizi@gmx.de>
2025-04-17 20:00:57 +0200

    Proxy Server Feature

--- secrets.h
+++ secrets.h
@@ -1,3 +1,8 @@
-const char* ssid = "yourWIFI_SSID";
-const char* password = "yourWIFI_PASSWORD";
-const char * token = "Bearer yourverylonghomeassistanttoken,do_not_remove_Bearer_and_space_left!";
+const char* ssid = "Your_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* proxySSID = "Your_Android_Phone_Hotspot_SSID";
+const char* proxyPassword = "Your_Android_Phone_Hotspot_Password";
+String serverName = "http://192.168.2.43:8123/api/states/input_number.bluecat_received_signal_strength";
+String proxyServer = "192.168.192.3";
+int proxyPort = 8080;
+const char * token = "Bearer yourverylonghomeassistantLong-Lived Access Token";
